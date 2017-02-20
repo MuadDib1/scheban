3a5fec271247a9f6cb0eeeb1e25f31a6d7e4d75e
MuadDib1 <yushuan_shieh@yahoo.com>
2017-02-20 18:01:58 +0900

    一日目の終わり

---
+++ application/plugins/slack.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+import sys
+from slackbot.bot import respond_to
+sys.path.append('..')
+from google_calendar import events2text
+
+
+@respond_to('今後の予定を教えて')
+def respond_schedule(message):
+    calendar_id = 'n2guckpa3df7imegmbk3k3r830@group.calendar.google.com'
+    reply_message = events2text(calendar_id=calendar_id)
+    message.reply(reply_message)
